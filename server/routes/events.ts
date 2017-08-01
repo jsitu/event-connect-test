75f6cb041d4a656da2fd478bbc68b7d54729a644
Johnny <jszeto0724@gmail.com>
2017-07-31 21:23:16 -0400

    A few fixes.

--- server/routes/events.ts
+++ server/routes/events.ts
@@ -5,7 +5,7 @@ const eventsRouter: Router = Router();
 const getEventsRouter = (org: any) => {
     eventsRouter.get('/', function(req, res) {
         // tslint:disable-next-line:max-line-length
-        const q = 'SELECT Id, Title__c, Start__c, End__c, Status__c, Address__c, City__c, State__c, Zip__c, Cover_Image_URL__c, Remaining_Seats__c, Description__c, Event_ID__c, (SELECT Session_ID__c, Title__c, Start__c, End__c, Remaining_Seats__c, Status__c FROM Sessions__r) FROM Event__c';
+        const q = 'SELECT Id, Title__c, Start__c, End__c, Status__c, Address__c, City__c, State__c, Zip__c, Cover_Image_URL__c, Remaining_Seats__c, Registration_Limit__c, Description__c, Event_ID__c, (SELECT Session_ID__c, Title__c, Start__c, End__c, Remaining_Seats__c, Registration_Limit__c, Status__c FROM Sessions__r) FROM Event__c';
 
         org.query({query: q}, function (err, resp) {
             if (err) {
@@ -22,7 +22,7 @@ const getEventsRouter = (org: any) => {
         if (id) {
             // tslint:disable-next-line:max-line-length
             // tslint:disable-next-line:quotemark
-            const q = "SELECT Id, Title__c, Start__c, End__c, Status__c, Address__c, City__c, State__c, Zip__c, Cover_Image_URL__c, Remaining_Seats__c, Description__c, Event_ID__c, (SELECT Session_ID__c, Title__c, Start__c, End__c, Remaining_Seats__c, Status__c FROM Sessions__r) FROM Event__c WHERE Id = '" + id + "'";
+            const q = "SELECT Id, Title__c, Start__c, End__c, Status__c, Address__c, City__c, State__c, Zip__c, Cover_Image_URL__c, Remaining_Seats__c, Registration_Limit__c, Description__c, Event_ID__c, (SELECT Session_ID__c, Title__c, Start__c, End__c, Remaining_Seats__c, Registration_Limit__c, Status__c FROM Sessions__r) FROM Event__c WHERE Id = '" + id + "'";
 
             org.query({query: q}, function (err, resp) {
                 if (err) {
