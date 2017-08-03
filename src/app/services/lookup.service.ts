4ba7ede56cf9b94c15d01e20542348b7fbe14d13
Johnny <jszeto0724@gmail.com>
2017-08-03 12:10:04 -0400

    Host the default event image within Salesforce document.

--- src/app/services/lookup.service.ts
+++ src/app/services/lookup.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class LookupService {
 
-  defaultEventCoverImageUrl = 'http://www.alfredpolo.com/wp-content/uploads/2016/03/event.jpg';
+  defaultEventCoverImageUrl = 'https://c.na59.content.force.com/servlet/servlet.ImageServer?id=015f4000000zcvj&oid=00Df4000000nKw6&lastMod=1501765598000';
 
   constructor() { }
 
